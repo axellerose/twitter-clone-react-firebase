27e8e64d8f631fd25a3b09fa7707661a698dc75e
Alex <alex@Alexs-MacBook-Pro.local>
2020-10-27 16:10:44 +0100

    sidebar without functionality done

---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import SidebarOption from './SidebarOption'
+
+import '../styles/Sidebar.css'
+import TwitterIcon from "@material-ui/icons/Twitter"
+import {Home, Search, NotificationsNone, MailOutline, BookmarkBorder, ListAlt, PermIdentity, MoreHoriz} from '@material-ui/icons'
+import { Button } from '@material-ui/core'
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <TwitterIcon />
+
+            <SidebarOption Icon={Home} text="Home" active/>
+            <SidebarOption Icon={Search} text="Explore" />
+            <SidebarOption Icon={NotificationsNone} text="Notifications" />
+            <SidebarOption Icon={MailOutline} text="Messages" />
+            <SidebarOption Icon={BookmarkBorder} text="Bookmarks" />
+            <SidebarOption Icon={ListAlt} text="Lists" />
+            <SidebarOption Icon={PermIdentity} text="Profile" />
+            <SidebarOption Icon={MoreHoriz} text="More" />
+            
+            <Button variant="outlined" className="sidebar__tweet" fullWidth>Tweet</Button>
+        </div>
+    )
+}
+
+export default Sidebar
